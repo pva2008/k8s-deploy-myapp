9308d5e649de69ddf28221445ee18e7de361154a
Vasili Pasmurtsev <pva2008@gmail.com>
2023-12-26 23:59:03 +0300

    df

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo  docker build \
+sudo -E  docker build \
 --build-arg http_proxy=http://172.16.10.29:8080/ \
 --build-arg https_proxy=http://172.16.10.29:8080/ \
 --build-arg no_proxy=localhost,127.0.0.1,0.0.0.0,10.0.0.0/8,172.16.10.0/24,192.168.155.0/24,192.168.65.0/24 \
