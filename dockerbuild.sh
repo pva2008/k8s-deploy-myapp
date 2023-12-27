741c004d87c0f8c0e78a05a76c4d72d4da66494c
Vasili Pasmurtsev <pva2008@gmail.com>
2023-12-27 13:32:33 +0300

    df

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 sudo -E docker login;
-sudo -E docker build -t pva2008/k8sphp:latest . ;
+sudo  docker build -t pva2008/k8sphp:latest . ;
+--build-arg http_proxy=http://172.16.10.29:8080/ \
+--build-arg https_proxy=http://172.16.10.29:8080/ \
+--build-arg no_proxy=localhost,127.0.0.1,0.0.0.0,10.0.0.0/8,172.16.10.0/24,192.168.155.0/24,192.168.65.0/24 \
+--build-arg HTTP_PROXY=http://172.16.10.29:8080/ \
+--build-arg HTTPS_PROXY=http://172.16.10.29:8080/ \
+--build-arg NO_PROXY=localhost,127.0.0.1,0.0.0.0,10.0.0.0/8,172.16.10.0/24,192.168.155.0/24,192.168.65.0/24 \
+-t pva2008/k8sphp:latest . ;
 sudo -E docker push  pva2008/k8sphp:latest ;
