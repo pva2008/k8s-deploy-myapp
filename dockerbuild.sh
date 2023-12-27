e419504b77b8e12987a0ddf1564150b3d604935d
Vasili Pasmurtsev <pva2008@gmail.com>
2023-12-27 12:41:36 +0300

    df

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo -E docker login ;cd  /home/jenkins/ ; ./dockerbuild.sh ; docker push  pva2008/k8sphp:latest ;
+sudo -E docker login ; cd  /home/jenkins/ ; ./dockerbuild.sh ; docker push  pva2008/k8sphp:latest ;
