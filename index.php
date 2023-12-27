a04898e8f02ba81ff9302600834212550c8577f9
Vasili Pasmurtsev <pva2008@gmail.com>
2023-12-27 14:23:14 +0300

    dfd16

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $ip_server = $_SERVER['SERVER_ADDR'];
 echo "<h1 bac>Hello from K8S-CLUSTER \"Test-BelVeb\"</h1><br>";
 echo "Server IP Address is: $ip_server", "<br><p>"; 
 echo "Made by <font color=blue>Vasili Pasmurtsev", "<br><p>";
-echo "<font color=red>Version X";
+echo "<font color=red>Version X for Ivan";
 echo "<body style='background-color:maroon'>";
 
 ?>
