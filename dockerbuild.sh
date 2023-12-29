e5e504c822c1eaa00c6fd339c8a21d3ed320a450
Vasili Pasmurtsev <pva2008@gmail.com>
2023-12-29 16:44:11 +0300

    dfd16

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-sudo export http_proxy="http://172.16.10.29:8080/";
-sudo export https_proxy="http://172.16.10.29:8080/";
-sudo export ftp_proxy="http://172.16.10.29:8080/";
-sudo export no_proxy="localhost,127.0.0.1,0.0.0.0,10.0.0.0/8,172.16.10.0/24,192.168.155.0/24,192.168.65.0/24";
+92.168.65.0/24";
 
 echo "======================================================";
-sudo printenv;
+sudo -E printenv;
 echo "======================================================";
-sudo docker info;
+sudo -E docker info;
 echo "======================================================";
 
 sudo  docker build -t pva2008/k8sphp:latest . ;
