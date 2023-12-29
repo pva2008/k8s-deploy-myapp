649b2b8a1620c8e524fc406d53435b10de3a551e
Vasili Pasmurtsev <pva2008@gmail.com>
2023-12-29 16:08:28 +0300

    dfd16

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+echo "======================================================";
+sudo printenv;
+echo "======================================================";
+sudo docker info;
+echo "======================================================";
 
 sudo  docker build -t pva2008/k8sphp:latest . ;
 sudo  docker push  pva2008/k8sphp:latest;
