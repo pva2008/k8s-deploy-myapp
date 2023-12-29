fb61892fe0c70b63f8298f7ba8991b5e676a942e
Vasili Pasmurtsev <pva2008@gmail.com>
2023-12-29 16:40:33 +0300

    dfd16

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-sudo echo "export http_proxy="http://172.16.10.29:8080/";
-export https_proxy="http://172.16.10.29:8080/";
-export ftp_proxy="http://172.16.10.29:8080/";
-export no_proxy="localhost,127.0.0.1,0.0.0.0,10.0.0.0/8,172.16.10.0/24,192.168.155.0/24,192.168.65.0/24"
-# For curl
+sudo export http_proxy="http://172.16.10.29:8080/";
+sudo export https_proxy="http://172.16.10.29:8080/";
+sudo export ftp_proxy="http://172.16.10.29:8080/";
+sudo export no_proxy="localhost,127.0.0.1,0.0.0.0,10.0.0.0/8,172.16.10.0/24,192.168.155.0/24,192.168.65.0/24";
+
 echo "======================================================";
 sudo printenv;
 echo "======================================================";
