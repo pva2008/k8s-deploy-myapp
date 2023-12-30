6964c125c1a58c6732d67cb91ac309f911f46858
Vasili Pasmurtsev <pva2008@gmail.com>
2023-12-30 15:43:44 +0300

    dfd16

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export DOCKER_BUILDKIT=0;
-export COMPOSE_DOCKER_CLI_BUILD=0;
+#export DOCKER_BUILDKIT=0;
+#export COMPOSE_DOCKER_CLI_BUILD=0;
 echo "======================================================";
 
 #//sudo -E printenv;
